c48ee95ab4a06736656f73f4c8a534a7d3879f1c
Andras Mihaly <andrasm96@gmail.com>
2019-10-09 12:34:58 -0700

    Initial test commit

--- hash_table.py
+++ hash_table.py
@@ -1,9 +1,12 @@
 # HashTable: An unordered key-value data structure providing O(1) store, retrieve
 # search and delete operations.
 # Your implementation should pass the tests in test_hash_table.py.
-# YOUR NAME
+# Andras Mihaly
 
 
 class HashTable:
 
     pass
+
+    def __init__(self):
+        self.size = 10
