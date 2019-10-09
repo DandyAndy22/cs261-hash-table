cf4d64f88f8eea7179394ce6c85127067d3aac06
Andras Mihaly <andrasm96@gmail.com>
2019-10-09 13:20:10 -0700

    Another one bites the dust

--- hash_table.py
+++ hash_table.py
@@ -8,5 +8,17 @@ class HashTable:
 
     pass
 
-    def __init__(self):
-        self.size = 10
+    def __init__(self, size=10):
+        self.size = size
+        self.data = []
+        self.keys = []
+
+    def __setitem__(self, key, value):
+        #OR .append(value)
+        self.data = value
+        self.keys = key
+    
+    def __getitem__(self, key):
+        for k in self.keys:
+            if k == key:
+                return self.data[k]
